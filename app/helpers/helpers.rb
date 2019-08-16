3227499ae72aab7af1bcd8fd775970546e33abf3
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-16 11:25:13 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,11 @@
 class Helpers
   
-  def current_user()
+  def current_user(session)
+    @user = User.find(session[:user_id])
+    return  @user
   end
   
-  def is_logged_in?
+  def is_logged_in?(session)
+    if 
   end
 end

19301a04959197e7a6cc7cd4a202feffbf9f5571
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-16 11:48:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,17 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params["username"])
-    if @user
-      session[:user_id] = @user.id
+    user = User.find_by(username: params["username"])
+    if user
+      session[:user_id] = user.id
       redirect to '/account'
     else
-      erb :'/views/error'
+      erb :error
     end
   end
 
   get '/account' do
-    erb :'/views/account'
+    erb :account
   end
 
   get '/logout' do

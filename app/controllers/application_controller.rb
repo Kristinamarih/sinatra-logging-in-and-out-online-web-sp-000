01bee9ce92cdf7148721b980cdaeffe15a2d29d3
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-16 11:16:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params["username"])
     @user.save
     session[:user_id] = @user.id
-    
+    redirect to '/account'
   end
 
   get '/account' do
